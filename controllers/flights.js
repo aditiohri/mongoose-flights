d7b109b3693df821c26019f20d22f501e60a97e1
Aditi Ohri <didiohri@gmail.com>
2019-11-16 15:57:01 -0600

    add flight date

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,7 +8,7 @@ module.exports = {
 
 function index(req, res) {
     Flight.find({}, function(err, flights) {
-        res.render('flights/index', {flights});
+        res.render('flights', {flights});
     });
 }
 
@@ -16,13 +16,12 @@ function create(req, res) {
     var flight = new Flight(req.body);
     flight.save(function(err) {
         if (err) return res.render('flights/new');
-        res.redirect('flights/index');
+        console.log(flight);
+        res.redirect('flights');
     });
 }
 
 function newFlight(req, res) {
     var flight = new Flight(req.body);
-    res.render('flights/new', {
-        default: flight.departs.default
-    });
+    res.render('flights/new');
 }
