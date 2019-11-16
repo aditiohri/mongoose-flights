aa7da4d56c4291a004e89457df0b1a306bf289ad
Aditi Ohri <didiohri@gmail.com>
2019-11-16 02:12:01 -0600

    add flights index and new

---
+++ models/flight.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    flightNum: {
+        type: Number,
+        required: true,
+        max: 9999,
+        min: 10,
+    },
+    departure: {
+        type: Date,
+        default: function(){
+            let today = new Date();
+            return new Date(today.setFullYear(today.getFullYear() + 1))
+        }
+    }
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
