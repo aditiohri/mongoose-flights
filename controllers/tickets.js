37e1b380d2f142f1cbd6f24ea271aea96efda206
Aditi Ohri <didiohri@gmail.com>
2019-11-19 19:10:50 -0600

    update create ticket functionality

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -11,9 +11,23 @@ function newTicket(req, res){
 };
 
 function create(req, res){
+    console.log('req.body.flight: ', req.body.flight);
     console.log('req.body: ', req.body);
-    console.log('req.params.id: ', req.params.id);
-    Ticket.create(req.body, function(err){
-        res.redirect(`/flights/${req.params.id}`);
+    req.body.flight = req.params.id;
+    console.log('req.body.flight AFTER:', req.body.flight)
+    console.log('req.params.id AFTER:', req.params.id)
+    Ticket.create(req.body, function(err, ticket){
+        console.log('req.body.flight after ticket:', req.body.flight)
+        console.log('req.params.id after ticket:', req.params.id)
+        console.log('ticket.flight: ', ticket.flight);
+        res.redirect(`flights/${ticket.flight}`)
     })
 }
+
+// function create(req, res){
+//     console.log('req.body: ', req.body);
+//     console.log('req.params.id: ', req.params.id);
+//     Ticket.create(req.body, function(err){
+//         res.redirect(`/flights/${req.params.id}`);
+//     })
+// }
